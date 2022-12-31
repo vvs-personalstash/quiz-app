7647fe75c9079a5b27b0945fe0ea9d9b58d0780d
vvs-personalstash <viralkverma0987@gmail.com>
2022-12-31 21:17:46 +0530

    This app is a app that gives you a quiz andresults

---
+++ lib/questions.dart
@@ -0,0 +1,8 @@
+class Question {
+  late String questionText;
+  late bool questionanswer;
+  Question({required String q, required bool a}) {
+    questionText = q;
+    questionanswer = a;
+  }
+}
